32bb8739dad6aff887d09e494a432db912dcf404
Christian Bedo <thierry.moa.1410@gmail.com>
2023-04-06 13:21:42 -0400

    init commit with dependacy

---
+++ affecationf/src/main/java/com/mandat/affecationf/AffecationfApplication.java
@@ -0,0 +1,13 @@
+package com.mandat.affecationf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffecationfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffecationfApplication.class, args);
+	}
+
+}
---
+++ affecationf/src/test/java/com/mandat/affecationf/AffecationfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandat.affecationf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffecationfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
